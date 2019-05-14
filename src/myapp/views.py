bb4fe4eb437f7f5c7707fa856bb150646b8cdc36
sergey <sergey.kostut740@gmail.com>
2019-05-14 21:43:44 +0300

    form

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('hello world')
+def index1(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        return HttpResponse('len is {}'.format(len(name)))
+    return HttpResponse('Its GET request')
