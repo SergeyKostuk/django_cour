ddc47a3a2cf7d8d744e5fe8122fecb564723ee6a
sergey <sergey.kostut740@gmail.com>
2019-05-21 21:34:16 +0300

    validation

--- src/cw19/views.py
+++ src/cw19/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import PostForms
 from django.http import HttpResponse
 
 
@@ -8,11 +8,17 @@ def record(request):
     if request.method == 'GET':
         return render(request, 'record_line.html')
     if request.method == 'POST':
-        firstname = request.POST.get('name')
-        lastname = request.POST.get('lastname')
-        age = request.POST.get('age')
-        comment = request.POST.get('comment')
+        form = PostForms(request.POST)
+        if form.is_valid():
+            data = form.clean_data
+            firstname = data.POST.get('name')
+            lastname = data.POST.get('lastname')
+            age = data.POST.get('age')
+            comment = data.POST.get('comment')
 
-        print(f'{firstname}|{lastname}|{age}|{comment}\n')
+            print(f'{firstname}|{lastname}|{age}|{comment}\n')
 
-        return render(request, 'record_line.html')
+            return render(request, 'record_line.html')
+        else:
+            errors = form.errors
+            return HttpResponse(f'{errors}')
