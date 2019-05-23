d8a897697122eae2f310a932ac1bf1ade180c414
sergey <sergey.kostut740@gmail.com>
2019-05-23 21:08:31 +0300

    requarements

--- src/cw20/models.py
+++ src/cw20/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    age = models.IntegerField()
+    profession = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.firstname
